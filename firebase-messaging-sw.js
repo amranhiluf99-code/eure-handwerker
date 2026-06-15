35f1c3dc1d43d894e12180005375a18840928231
amranhiluf99-code <amranhiluf99@gmail.com>
2026-06-15 14:22:53 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,21 +2,21 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCmT5DNOZK__ytivq54ClbiQNMvv5bwrTQ",
-  authDomain: "arbeit-app-ff263.firebaseapp.com",
-  projectId: "arbeit-app-ff263",
-  storageBucket: "arbeit-app-ff263.firebasestorage.app",
-  messagingSenderId: "337916056955",
-  appId: "1:337916056955:web:99650927097d363d0019de"
+  apiKey: "AIzaSyBNkBdFffJxV8Kj20PYX5YGZPQYlwla4nY",
+  authDomain: "pubg-freefire-ttopup.firebaseapp.com",
+  projectId: "pubg-freefire-ttopup",
+  storageBucket: "pubg-freefire-ttopup.firebasestorage.app",
+  messagingSenderId: "76395303308",
+  appId: "1:76395303308:web:ae83f116d29692490cc6a9"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-  const title = payload.notification?.title || 'Handwerker-Portal';
+  const title = payload.notification?.title || 'Eure Handwerker';
   self.registration.showNotification(title, {
     body: payload.notification?.body || '',
-    icon: '/icon-192.png',
-    badge: '/icon-192.png'
+    icon: './icon-192.png',
+    badge: './icon-192.png'
   });
 });
